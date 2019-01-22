76482980ba86bfb1b0aa1ee7059cee091c0a60ce
LatvianProgrammer <kalvans.rolands@gmail.com>
2019-01-22 20:33:59 +0200

    redid naming

--- download/uffenheim.py
+++ download/uffenheim.py
@@ -3,9 +3,9 @@ from utility.data_download import next_date
 
 
 def download_links(base_path):
-
     base_url = 'https://www.wind-erleben.de/ajaxdata/data/all/{0}/{0}/'
-    inputs = [(Path(base_path, '{}.json'.format(file)), base_url.format(file))
-              for file in (ayy.strftime('%Y-%m-%d') for ayy in next_date())]
+    inputs = [(Path(base_path, '{}.json'.format(formatted_date)), base_url.format(formatted_date))
+              for formatted_date in (current_date.strftime('%Y-%m-%d')
+                                     for current_date in next_date())]
 
     return inputs
