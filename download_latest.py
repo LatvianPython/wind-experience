d316c024166508f3390d4c1fb46fd6d2b260ef26
LatvianProgrammer <kalvans.rolands@gmail.com>
2019-01-23 20:06:01 +0200

    moving thngs around a bit

--- download_latest.py
+++ download_latest.py
@@ -12,12 +12,11 @@ def main():
     for source, cookies in [('uffenheim', None),
                             ('wunderground', None),
                             ('timeanddate', {'TIMEANDDATE': 'fud_1:fup_1:fut_1:fuw_1:fur_1'})]:
-        download_module = __import__('download_url_generator', fromlist=[source])
-        download_url_generator = getattr(download_module, source)
+        download_url_generator = getattr(__import__('download_url_generator', fromlist=[source]), source)
+        parsing_function = getattr(__import__('parsing', fromlist=[source]), source)
+
         links = download_url_generator(base_data_dir)
         download_all(inputs=links, cookies=cookies)
-        parsing_module = __import__('parsing', fromlist=[source])
-        parsing_function = getattr(parsing_module, source)
         parsing_function(raw_data_path=base_data_dir / source,
                          parsed_data_path=parsed_data_dir / (source + '.csv'))
 
